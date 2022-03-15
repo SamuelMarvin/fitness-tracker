a24f8ad5cdc1c659e1a7a383841d652a55289e61
Samuel Marvin <samuelmarvin@Samuels-MacBook-Pro-2.local>
2022-03-15 14:09:28 -0500

    post

--- public/app.js
+++ public/app.js
@@ -24,6 +24,6 @@ $('.testButton4').on('click', ()=>{
 
 
 $('.postnewuser').on('click', ()=>{
-    $.post('/users',{ name:katy, username:katyb,})
+    $.post('/users',{ name:'katy', username:'katyb',})
     .done((data)=> console.log(data), "json")
 })
